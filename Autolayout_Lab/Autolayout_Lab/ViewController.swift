8664a6f7777bd8df67264f0ce6c59aeaf91ca069
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-08 17:11:38 -0500

    completed Q 1-4

---
+++ Autolayout_Lab/Autolayout_Lab/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Autolayout_Lab
+//
+//  Created by Amy Alsaydi on 11/7/19.
+//  Copyright © 2019 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var tealView: UIView!
+    @IBOutlet weak var purpleView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
